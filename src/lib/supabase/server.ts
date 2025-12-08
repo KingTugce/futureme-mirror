4252bef58b3d44e3224d60c6759d133cecf211a9
KingTugce <kingstugce@gmail.com>
2025-12-07 20:04:57 -0800

    UPDATE

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,4 +1,4 @@
-// lib/supabase/server.ts
+//   lib/supabase/server.ts
 // -----------------------------
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
